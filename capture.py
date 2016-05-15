a3cad108b1c786d9d98e9733ffd15ced764cdffc
Christopher Greene <archgoon@gmail.com>
2016-05-15 11:53:03 -0700

    Now can accumulate data over multiple chunks.
    Also, capture.py is now an executable.

--- capture.py
+++ capture.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
 import pyaudio
 import numpy
 import itertools
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 48000
-CHUNK = 1024*8
+RATE = 44100*4
+CHUNK = 1024*4
+BUFFERS = 5
 RECORD_SECONDS = 5
 
 audio = pyaudio.PyAudio()
@@ -16,14 +18,24 @@ stream = audio.open(format=pyaudio.paInt16,
         input=True,
         frames_per_buffer=CHUNK)
 
-for display in itertools.cycle([False, True]):
+for display in itertools.cycle(range(BUFFERS)):
     data = stream.read(CHUNK)
-    a = numpy.fromstring(data, dtype=numpy.int16)
-    delta_t = float(CHUNK)/RATE
-    a = a - numpy.mean(a)
-    fft = numpy.fft.fft(a)
-    fft[CHUNK/2:] = 0
-    print "%.3f" % (numpy.argmax(numpy.abs(fft))/delta_t)
+    data = numpy.fromstring(data, dtype=numpy.int16)
+    if not display:
+        a = data
+    else:
+        a = numpy.append(a, data)
+    if (display + 1) % BUFFERS == 0:
+        delta_t = (float(CHUNK)* BUFFERS)/RATE
+        a = a - numpy.mean(a)
+        fft = numpy.fft.fft(a)
+        fft[CHUNK/2:] = 0 # prevents alias from being detected
+        afft = numpy.abs(fft)
+        amax = numpy.argmax(afft)
+        print "%.3f %.3f (%.3f-%.3f)" % (amax/delta_t,
+            numpy.max(afft),
+            (amax-1)/delta_t,
+            (amax+1)/delta_t)
 
 stream.stop_stream()
 stream.close()
