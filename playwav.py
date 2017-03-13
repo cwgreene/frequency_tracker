8ca5aadcc2c5fe8447555d95e5eb164756ffbe00
Chris Greene <archgoon@gmail.com>
2017-03-13 08:32:53 -0700

    Basic support for looping
    In future, should probably do something more interesting,
    like play multiple files repeatedly.

--- playwav.py
+++ playwav.py
@@ -10,6 +10,33 @@ import pyaudio
 
 CHUNK = 1024
 
+def playfile(wavefile, audio, forever=False):
+    """
+    plays an opened wavefile (wave.open) using audio interface.BaseException
+
+    wavefile - wave file opened with wave.open
+    audio - pyaudio interface
+    forever - (default False) whether to loop forever.
+    """
+    output = audio.open(
+        format=audio.get_format_from_width(wavefile.getsampwidth()),
+        channels=wavefile.getnchannels(),
+        rate=wavefile.getframerate(),
+        output=True)
+
+
+    while True:
+        data = wavefile.readframes(CHUNK)
+        while len(data) > 0:
+            output.write(data)
+            data = wavefile.readframes(CHUNK)
+        if not forever:
+            break
+        wavefile.rewind()
+
+    output.stop_stream()
+    output.close()
+
 def main(args):
     """
     Plays a wavefile
@@ -18,26 +45,12 @@ def main(args):
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
+    parser.add_argument("--loop", action="store_true")
     options = parser.parse_args(args)
     wavefile = wave.open(options.file, 'rb')
 
     audio = pyaudio.PyAudio()
-
-    output = audio.open(
-        format=audio.get_format_from_width(wavefile.getsampwidth()),
-        channels=wavefile.getnchannels(),
-        rate=wavefile.getframerate(),
-        output=True)
-
-    data = wavefile.readframes(CHUNK)
-
-    while len(data) > 0:
-        output.write(data)
-        data = wavefile.readframes(CHUNK)
-
-    output.stop_stream()
-    output.close()
-
+    playfile(wavefile, audio, options.loop)
     audio.terminate()
 
 if __name__ == "__main__":
